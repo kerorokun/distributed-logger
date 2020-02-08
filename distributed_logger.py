e9729f64bbc9dd5ba3502a8639d694713298f4fe
Andrew Gao <andrewg9@illinois.edu>
2020-02-07 19:50:10 -0600

    Added delay calculations.

--- distributed_logger.py
+++ distributed_logger.py
@@ -1,13 +1,13 @@
 import argparse
 import socket
 import time
-
+import os
 import multiprocessing as mp
 
 parser = argparse.ArgumentParser(description="Create a logger to listen for messages on a specified port.")
 parser.add_argument('port', nargs=1, default=1234, type=int)
 
-def handle_client(conn, address, queue):
+def handle_client(conn, address, log_queue, delay_queue):
     name = None
     with conn:
         # Handle messages
@@ -20,18 +20,56 @@ def handle_client(conn, address, queue):
                 
                 if name is None:
                     name = msg
-                    queue.put(f'{time.time()} - {name} connected')
+                    log_queue.put(f'{time.time()} - {name} connected')
                 else:
                     msg_time, msg = msg.split()
 
-                    # TODO: Put delay onto a queue
-                    delay = time.time() - float(msg_time)
+                    delay_queue.put(time.time() - float(msg_time))
+                    log_queue.put(f'{msg_time} | {name} | {msg}')
                     
-                    queue.put(f'{msg_time} | {name} | {msg}')
         except Exception as e:
             # TODO: Better error handling
             print(e)
-    queue.put(f'{time.time()} - {name} disconnected')
+    log_queue.put(f'{time.time()} - {name} disconnected')
+
+
+
+def calculate_delay_metrics(delay_queue, log_queue):
+    # Get the number of events per second
+    # Min, Max, Median, 90th percentile
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file = os.path.join(curr_dir, 'delay_log.txt')
+
+    with open(log_file, 'w') as fp:
+    
+        while True:
+            time.sleep(1)
+            
+            # TODO: Utilize numpy / pandas
+            num_events = delay_queue.qsize()
+            events = []
+            try:
+                total_delay = 0
+                for i in range(num_events):
+                    delay = delay_queue.get()
+                    total_delay += delay
+                    events.append(delay)
+
+                if events:
+                    events.sort()
+
+                    if len(events) % 2 == 0: 
+                        median1 = events[len(events)//2] 
+                        median2 = events[len(events)//2 - 1] 
+                        median = (median1 + median2)/2
+                    else: 
+                        median = events[len(events)//2]
+
+                    fp.write(f'{events[0]}, {events[-1]}, {median}, {0.9 * total_delay} \n')
+                    log_queue.put(f'{events[0]}, {events[-1]}')
+                
+            except Exception as e:
+                print(e)
     
 
 def print_messages(queue):
@@ -49,19 +87,26 @@ def main():
     processes = []
 
     try:
-        process_queue = mp.Queue()
-        reader = mp.Process(target=print_messages, args=((process_queue), ))
-        processes.append(reader)
-        reader.start()
+        log_queue = mp.Queue()
+        delay_queue = mp.Queue()
+        
+        log_reader = mp.Process(target=print_messages, args=((log_queue), ))
+        processes.append(log_reader)
+        log_reader.start()
+        
+        delay_reader = mp.Process(target=calculate_delay_metrics, args=(delay_queue, log_queue))
+        processes.append(delay_reader)
+        delay_reader.start()
         
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            process_queue.put(f'Opening server at {host_ip}:{port}')
+            log_queue.put(f'Opening server at {host_ip}:{port}')
             s.bind((host, port))
             s.listen()
 
             while True:
                 conn, address = s.accept()
-                p = mp.Process(target=handle_client, args=(conn, address, process_queue))
+                p = mp.Process(target=handle_client,
+                               args=(conn, address, log_queue, delay_queue))
                 processes.append(p)
                 p.start()
             
@@ -70,7 +115,7 @@ def main():
             process.terminate()
             process.join()
         
-        print('\nServer closed')
+        print('\nServer closed. Creating the metrics graph.')
 
         
 
